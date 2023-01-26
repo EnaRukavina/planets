3fe4f653042c5b42e3fe239beef866c6bc0cdf40
Ena Rukavina <ena.rukavina@gmail.com>
2023-01-26 22:24:52 +0100

    add assets, fix image import ts error

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-import styled from 'styled-components/macro';
+import styled, { ThemeProvider } from 'styled-components/macro';
 import './App.css';
+import { Header } from './components/Header';
+import { theme } from './theme';
+import stars from './assets/images/background-stars.svg';
 
-const Container = styled.div``;
+const AppContainer = styled.div`
+  height: 100vh;
+  width: 100%;
+  background: black;
+  background-image: ${`url(${stars})`};
+  background-size: cover;
+  background-repeat: no-repeat;
+`;
 
 function App() {
-  return <Container></Container>;
+  return (
+    <ThemeProvider theme={theme}>
+      <AppContainer>
+        <Header />
+        <Header />
+        <Header />
+        <Header />
+      </AppContainer>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/theme.ts
@@ -0,0 +1,16 @@
+export const theme = {
+  colors: {
+    white: '#ffffff',
+    black: '#070724',
+    darkGray: '#38384F',
+    lightGray: '#838391',
+    yellow: '#EDA249',
+    orange: '#CD5120',
+    red: '#D83A34',
+    darkOrange: '#D14C32',
+    teal: '#1ec2a4',
+    lightBlue: '#419EBB',
+    darkBlue: '#2d68f0',
+    purple: '#6f2ed6',
+  },
+};
